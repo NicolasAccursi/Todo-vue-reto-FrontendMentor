06fd1261720a4153a204f96082ecaae76df33648
Nicolas <accursinicolas@gmail.com>
2021-02-08 14:51:35 -0300

    iniciando prubeas de todo

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>ToDos</h1>
+  <TodoForm />
+</template>
+
+<script>
+import { provide, ref } from "vue";
+import TodoForm from "./TodoForm.vue";
+export default {
+  components: { TodoForm },
+  setup() {
+    const todos = ref([]);
+    provide("todos", todos);
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="formulario">
+    <input
+      type="text"
+      class="form-control my-3"
+      placeholder="Ingrese tarea"
+      v-model="texto"
+    />
+  </form>
+</template>
+
+<script>
+import { inject, ref } from "vue";
+export default {
+  setup() {
+    const todos = inject("todos");
+
+    const texto = ref("");
+
+    const formulario = () => {
+      console.log(texto.value);
+    };
+    return { formulario, texto };
+  },
+};
+</script>
+
+<style></style>
